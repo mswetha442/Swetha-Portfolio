0fdf962a09605b6fba6642eb9ea8241cfcded3b5
mswetha442 <mswetha442@gmail.com>
2026-09-06 20:30:47 +0530

    Register portfolio models in admin

--- main/main/admin.py
+++ main/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Project, Experience, Education
 
 
 @admin.register(Profile)
@@ -11,3 +11,19 @@ class ProfileAdmin(admin.ModelAdmin):
         "phone",
         "location",
     )
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "created_at")
+    search_fields = ("title", "technologies")
+
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ("job_title", "company", "start_date", "end_date")
+
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ("degree", "institution", "start_year", "end_year")
