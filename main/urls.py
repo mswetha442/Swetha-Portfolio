2886106949b8734f1a27423fcc7cae1c8098520d
mswetha442 <mswetha442@gmail.com>
2026-09-09 20:17:11 +0530

    Fix NoReverseMatch: Add login and logout URL patterns

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from django.contrib.auth import views as auth_views
+from .views import home, portfolio
 
 urlpatterns = [
-    path("", home, name="home"),
+    path('', home, name='home'),
+    path('p/<str:username>/', portfolio, name='portfolio'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
