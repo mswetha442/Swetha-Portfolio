83fe969f7ac26e4b7f895f17ab1af544d852e721
mswetha442 <mswetha442@gmail.com>
2026-09-09 14:01:57 +0530

    Update Cloudinary settings with active key

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -19,7 +19,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
 ]
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'lcuptldi',
+    'API_KEY': '835293999235681',
+    'API_SECRET': '2mnwh_f5efGbQR9y7-cyAWD3t50',
+}
 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
