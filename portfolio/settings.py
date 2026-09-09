a584397d97d59e9149f83180b82b5af86b244665
mswetha442 <mswetha442@gmail.com>
2026-09-09 19:25:39 +0530

    Fix module config, add superuser script, profile section, and project screenshot slideshow

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'portfolio.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 
 # Database
